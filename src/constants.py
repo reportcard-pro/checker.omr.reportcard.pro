96d4ab53d2c40cb20e6ddb9d6af6299c24f353e1
Syed Fazil Basheer <fazil@fazn.co>
2025-04-21 22:38:57 +0530

    Integrate for app.reportcard.pro

--- src/constants.py
+++ src/constants.py
@@ -37,6 +37,10 @@ FIELD_TYPES = {
         "bubbleValues": ["A", "B", "C", "D", "E"],
         "direction": "horizontal",
     },
+    "QTYPE_NUMERIC": {
+        "bubbleValues": ["/", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-"],
+        "direction": "vertical",
+    },
     #
     # You can create and append custom field types here-
     #
